d52e1007f7999e99e6a30963a951a35671cf11f8
Parth Parmar <parthmparmar@gmail.com>
2020-01-22 18:54:21 -0500

    fixed port error

--- server.js
+++ server.js
@@ -187,6 +187,6 @@ app.get("/mypodcasts", function(req, res){
 });
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
